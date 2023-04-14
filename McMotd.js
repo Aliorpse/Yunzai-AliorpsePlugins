a2c7c51538c05e17203047cf84997e87452106e3
Aliorpse <aliorpse@qq.com>
2023-04-14 23:27:09 +0000

    修复#Issue I6VVQW,修复bug
    Signed-off-by: Aliorpse <aliorpse@qq.com>

--- McMotd.js
+++ McMotd.js
@@ -2,8 +2,8 @@
 //开源地址https://gitee.com/Aliorpse/Yunzai-McMotd/
 import plugin from '../../lib/plugins/plugin.js'
 import _ from 'lodash'
-import fetch from 'node-fetch'
 import { segment } from "oicq"
+import fetch from 'node-fetch'
 const _path = process.cwd();
 
 export class McMotd extends plugin {
@@ -34,10 +34,21 @@ export class McMotd extends plugin {
         let res = await fetch("http://123.60.141.159/api/mcPing?ip=" + ip + "&port=" + port)
         if (!res) { return false }
         res = await res.json()
+        logger.mark(`${res}`)
+        if (res == false){
+            e.reply(`错误: 意外的返回数据(这通常是服务器的问题)`, true)
+            return
+        }
         if (res.hasOwnProperty("errMessage") === true) {
             e.reply(res.errMessage, true)
+            return
         }
-        let base64img = res.favicon.replace(/\\/g, "").replace(/data:image\/png;base64,/, "")
+        let serverimg = ""
+        if (res.hasOwnProperty("favicon") === true) {
+            serverimg = res.favicon.replace(/\\/g, "").replace(/data:image\/png;base64,/, "base64://")
+        }else{
+            serverimg = "http://123.60.141.159/api/mcPing/favicons/defaultServerIcon.png"
+        }
         let desc3 = ""
         let desc2 = "1"
         let desc1 = "1"
@@ -51,13 +62,12 @@ export class McMotd extends plugin {
                 type = res.modinfo.type
             }
             const message = [
-                segment.image(`base64://${base64img}`),
+                segment.image(`${serverimg}`),
                 `${desc1}`,
                 `\n----\n[版本] ${res.version.name}`,
                 `\n[协议] ${res.version.protocol}`,
                 `\n[类型] ${type}`,
                 `\n[玩家] ${res.players.online}/${res.players.max}`,
-                `\n[获取模式] 1`,
             ]
             e.reply(message, true)
         } else {
@@ -66,18 +76,22 @@ export class McMotd extends plugin {
             } else {
                 type = res.modinfo.type
             }
-            var length = res.description.extra.length;
-            for (var i = 0; i < length; i++) {
-                desc3 = desc3 + res.description.extra[i].text
+            if(res.description.hasOwnProperty("extra")){
+                var length = res.description.extra.length;
+                for (var i = 0; i < length; i++) {
+                    desc3 = desc3 + res.description.extra[i].text
+                }
+            }else{
+                desc2 = res.description.text + "\u00a7a"
+                desc3 = desc2.replace(/\u00a7+\w/g, "")
             }
             const message = [
-                segment.image(`base64://${base64img}`),
+                segment.image(`${serverimg}`),
                 `${desc3}`,
                 `\n----\n[版本] ${res.version.name}`,
                 `\n[协议] ${res.version.protocol}`,
                 `\n[类型] ${type}`,
                 `\n[玩家] ${res.players.online}/${res.players.max}`,
-                `\n[获取模式] 2`,
             ]
             e.reply(message, true)
         }
