58fe7aa9ac6db4869ec0dd4e65dbac1d7ac062d0
Aliorpse <aliorpse@qq.com>
2023-04-14 08:15:47 +0000

    优化，不再有图片缓存
    Signed-off-by: Aliorpse <aliorpse@qq.com>

--- McMotd.js
+++ McMotd.js
@@ -2,7 +2,6 @@
 //开源地址https://gitee.com/Aliorpse/Yunzai-McMotd/
 import plugin from '../../lib/plugins/plugin.js'
 import _ from 'lodash'
-import * as fs from 'fs'
 import { segment } from "oicq"
 const _path = process.cwd();
 
@@ -31,24 +30,19 @@ export class McMotd extends plugin {
             ip = port
             port = 25565
         }
-        let res = await fetch("http://123.60.141.159/?ip=" + ip + "&port=" + port) //自建API 不限请求次数,但导致我自己服务器卡顿的话火速关闭 基于php,Lib: https://github.com/xPaw/PHP-Minecraft-Query
+        let res = await fetch("http://123.60.141.159/?ip=" + ip + "&port=" + port)
         if (!res) { return false }
         res = await res.json()
         if (res.hasOwnProperty("errMessage") === true) {
             e.reply(res.errMessage, true)
-            return
         }
-        function base64_decode(base64str, file) { //抄的
-            var bitmap = new Buffer.from(base64str, 'base64');
-            fs.writeFileSync(file, bitmap);
-        }
-        base64_decode(res.favicon.replace(/\\/g, "").replace(/data:image\/png;base64,/, ""), "servericon.png") //base64解码服务器图片
+        let base64img = res.favicon.replace(/\\/g, "").replace(/data:image\/png;base64,/, "")
         let desc3 = ""
         let desc2 = "1"
         let desc1 = "1"
         let type = "1"
-        if (_.isString(res.description) === true) { //区分两种description形式
-            desc2 = res.description + "\u00a7a" //防止下一句报错，笨办法
+        if (_.isString(res.description) === true) {
+            desc2 = res.description + "\u00a7a"
             desc1 = desc2.replace(/\u00a7+\w/g, "")
             if (res.hasOwnProperty("modinfo") === false) {
                 type = "Vanilla"
@@ -56,7 +50,7 @@ export class McMotd extends plugin {
                 type = res.modinfo.type
             }
             const message = [
-                segment.image(_path + "/servericon.png"),
+                segment.image(`base64://${base64img}`),
                 `${desc1}`,
                 `\n----\n[版本] ${res.version.name}`,
                 `\n[协议] ${res.version.protocol}`,
@@ -72,11 +66,11 @@ export class McMotd extends plugin {
                 type = res.modinfo.type
             }
             var length = res.description.extra.length;
-            for (var i = 0; i < length; i++) { //笨办法，随便改
+            for (var i = 0; i < length; i++) {
                 desc3 = desc3 + res.description.extra[i].text
             }
             const message = [
-                segment.image(_path + "/servericon.png"),
+                segment.image(`base64://${base64img}`),
                 `${desc3}`,
                 `\n----\n[版本] ${res.version.name}`,
                 `\n[协议] ${res.version.protocol}`,
@@ -86,6 +80,5 @@ export class McMotd extends plugin {
             ]
             e.reply(message, true)
         }
-        return
     }
 }
