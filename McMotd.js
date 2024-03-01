1c9a350b0d3ea8290536476f5d578f1db77530f0
Aliorpse <aliorpse@qq.com>
2024-03-01 17:45:55 +0000

    使用api.mcstatus.io
    Signed-off-by: Aliorpse <aliorpse@qq.com>

--- McMotd.js
+++ McMotd.js
@@ -41,17 +41,21 @@ export class McMotd extends plugin {
         })
     }
     async addAlias(e) {
+
         var alias = fs.readFileSync("./data/McMotd/SAlias.json")
         alias = eval("(" + alias + ")")
         const content = e.message[0].text.slice(8)
+
         if (content == "") {
             e.reply('用法: #mcsadd [IP Address],仅限群聊',true)
             return
         }
+
         if(!e.isGroup) { 
             e.reply('该功能仅限群聊',true)
             return
         }
+
         if((e.sender.role == "admin" || e.sender.role == "owner" || e.isMaster)) {
             alias[`${e.group_id}`] = content
             fs.writeFileSync("./data/McMotd/SAlias.json",JSON.stringify(alias))
@@ -61,11 +65,17 @@ export class McMotd extends plugin {
             e.reply('该功能仅限群管理或主人',true)
             return
         }
+
     }
+
     async getMotd(e) {
+        let eulaBlocked = "否"
+        let srvRecord = "无"
+        let serverImg = ""
         var alias = fs.readFileSync("./data/McMotd/SAlias.json")
         alias = eval("(" + alias + ")")
         let content = e.message[0].text.slice(6)
+
         if (content == "") {
             if (e.group_id in alias){
                 content = alias[`${e.group_id}`]
@@ -76,82 +86,49 @@ export class McMotd extends plugin {
                 return
             }
           }
-        let ip = content.substring(0, content.indexOf(":"))
-        let port = content.replace(content.substring(0, content.indexOf(":") + 1), "")
-        if (port == content) {
-            ip = port
-            port = 25565
-        }
+
         let startime = performance.now()
-        let res = await fetch("http://123.60.141.159/api/mcPing?ip=" + ip + "&port=" + port)
+        let res = await fetch("https://api.mcstatus.io/v2/status/java/" + content)
         let endtime = performance.now()
-        let ms = `${(endtime - startime).toFixed(3)}`;
+        let ping = ((endtime - startime) / 1000).toFixed(2)
+
         if (!res) { return false }
+
         res = await res.json()
-        logger.mark(`${res}`)
-        if (res == false){
-            e.reply(`错误: 意外的返回数据(这通常是服务器的问题)`, true)
-            return
+
+        if (res.online == "false"){
+            e.reply(`错误: 服务器不在线\n查询IP: ${content}`,true)
+            return false
         }
-        if (res.hasOwnProperty("errMessage") === true) {
-            e.reply(res.errMessage, true)
-            return
-        }
-        let serverimg = ""
-        if (res.hasOwnProperty("favicon") === true) {
-            serverimg = res.favicon.replace(/\\/g, "").replace(/data:image\/png;base64,/, "base64://")
+
+        if (res.icon == null) {
+            serverImg = "https://api.mcstatus.io/v2/icon"
         }else{
-            serverimg = "http://123.60.141.159/api/mcPing/favicons/defaultServerIcon.png"
+            serverImg = res.icon.replace(/data:image\/png;base64,/, "base64://")
         }
-        let desc3 = ""
-        let desc2 = "1"
-        let desc1 = "1"
-        let type = "1"
-        if (_.isString(res.description) === true) {
-            desc2 = res.description + "\u00a7a"
-            desc1 = desc2.replace(/\u00a7+\w/g, "")
-            if (res.hasOwnProperty("modinfo") === false) {
-                type = "Vanilla"
-            } else {
-                type = res.modinfo.type
-            }
-            const message = [
-                segment.image(serverimg),
-                `${desc1}`,
-                `\n----\n[IP] ${content}`,
-                `\n[版本] ${res.version.name}`,
-                `\n[协议] ${res.version.protocol}`,
-                `\n[类型] ${type}`,
-                `\n[玩家] ${res.players.online}/${res.players.max}`,
-                `\n[请求耗时] ${ms}ms`,
-            ]
-            e.reply(message, true)
-        } else {
-            if (res.hasOwnProperty("modinfo") === false) {
-                type = "Vanilla"
-            } else {
-                type = res.modinfo.type
-            }
-            if(res.description.hasOwnProperty("extra")){
-                var length = res.description.extra.length;
-                for (var i = 0; i < length; i++) {
-                    desc3 = desc3 + res.description.extra[i].text
-                }
-            }else{
-                desc2 = res.description.text + "\u00a7a"
-                desc3 = desc2.replace(/\u00a7+\w/g, "")
-            }
-            const message = [
-                segment.image(serverimg),
-                `${desc3}`,
-                `\n----\n[IP] ${content}`,
-                `\n[版本] ${res.version.name}`,
-                `\n[协议] ${res.version.protocol}`,
-                `\n[类型] ${type}`,
-                `\n[玩家] ${res.players.online}/${res.players.max}`,
-                `\n[请求耗时] ${ms}ms`,
-            ]
-            e.reply(message, true)
+
+        if (res.srv_record != null) {
+            srvRecord = res.srv_record.host + ":" + res.srv_record.port
         }
+        
+        if (res.eula_blocked == true) {
+            eulaBlocked = "是"
+        }
+
+        const message = [
+            segment.image(serverImg),
+            res.motd.clean,
+            `\n----\n[IP] ${content}`,
+            `\n[版本] ${res.version.name_clean}`,
+            `\n[协议] ${res.version.protocol}`,
+            `\n[SRV记录] ${srvRecord}`,
+            `\n[Mojang屏蔽] ${eulaBlocked}`,
+            `\n[玩家] ${res.players.online}/${res.players.max}`,
+            `\n[请求耗时] ${ping}s`,
+        ]
+
+        e.reply(message,true)
+        return true
+
     }
 }
