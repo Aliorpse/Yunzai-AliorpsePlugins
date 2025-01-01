650b35efda268efbc153ce9c19435fcc0112146c
Aliorpse <aliorpse@qq.com>
2025-01-01 14:32:41 +0000

    适应bili2233.cn
    Signed-off-by: Aliorpse <aliorpse@qq.com>

--- bilitv.js
+++ bilitv.js
@@ -1,7 +1,7 @@
 import plugin from '../../lib/plugins/plugin.js'
 import _ from 'lodash'
 
-const regB23 = /b23\.tv\\?\/\w{7}/
+const regB23 = /(b23\.tv|bili2233.cn)\\?\/\w{7}/
 const regBV = /BV1\w{9}/
 const regAV = /av\d+/
 const regMD = /md\d+/ // media_id 番剧md号
